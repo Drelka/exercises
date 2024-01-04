9ad1b36b5431492ed94f36d61569f54b4424b839
Drelka <47503020+Drelka@users.noreply.github.com>
2024-01-04 13:32:56 +0000

    Add a while loop exercise
    Something I made unnecessarily complicated.

---
+++ loops/while/script.js
@@ -0,0 +1,25 @@
+// use while loop in this example
+
+// create html p element with an id of "para"
+
+// create an array
+// when button in clicked:
+    // append the array element
+    // keep appending arr. elem. from [0] to [array.lenght-1]
+        // after adding the last arr. element make the button inactive
+
+// array [i]
+// let i = 0;
+
+// while (i <= (array.length -1)){
+    // btn.addEventListener ("click", () => {
+        // append array[i];
+        // })
+    // i++
+// }
+
+const cats = ["Pete", "Biggles", "Jasmine"];
+
+const paraCats = document.querySelector("#paraCats");
+
+paraCats = cats.toString();
