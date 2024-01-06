e7e8785192bc791c0e0903ae586f00202421f043
Drelka <47503020+Drelka@users.noreply.github.com>
2024-01-06 15:25:29 +0000

    Finished exercise

--- loops/while/script.js
+++ loops/while/script.js
@@ -1,45 +1,26 @@
-// use while loop in this example
-
-// create html p element with an id of "para"
-
-// create an array
-// when button in clicked:
-    // append the array element
-    // keep appending arr. elem. from [0] to [array.lenght-1]
-        // after adding the last arr. element make the button inactive
-
-// array [i]
-// let i = 0;
-
-// while (i <= (array.length -1)){
-    // btn.addEventListener ("click", () => {
-        // append array[i];
-        // })
-    // i++
-// }
-
-const cats = ["Pete", "Biggles", "Jasmine"];
+const cats = ["Pete", "Biggles", "Jasmine", "Joora", "Amarant"];
 
 const paraCats = document.querySelector("#paraCats");
 const btn = document.querySelector("button");
 const para = document.querySelector("#para");
 
-paraCats.textContent = cats.join(", ");
+paraCats.textContent = "List of cats: " + cats.join(", ") + ".";
 
 let i = 0;
 
-while (i < (cats.length)) {
-    let cat = cats[i].toString();
-    
-    btn.addEventListener("click", () => {
-        if (i = cats.length -1){
-            para.textContent += `${cat}.`;
-        }
-        else {
+btn.addEventListener("click", () => { 
+    while (i < cats.length) {
+
+        let cat = cats[i].toString();
+
+        if (i < cats.length -1){
             para.textContent += `${cat}, `;
         }
-    })
-
-    i++;
-    console.log(i);
-};
+        else {
+            para.textContent += `${cat}.`;
+        }
+        i++;
+        
+        break;
+    };
+});
