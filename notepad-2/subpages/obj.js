c41fca0cfa07325d6bdc605e43008e3682e2930d
Drelka <47503020+Drelka@users.noreply.github.com>
2025-02-16 17:38:37 +0000

    just saving whatever was added

---
+++ notepad-2/subpages/obj.js
@@ -0,0 +1,56 @@
+// object syntax
+
+const myObject = {
+    property: "value",
+    "other property": function(x, y){
+        return x + y;
+    }
+};
+
+
+// 2 ways to get information from the object
+
+myObject.property;
+
+myObject["other property"];
+
+
+// can't use variables in dot notation
+
+myObject.variable; // undefined - looking for name: "variable"
+
+myObject[variable]; // equivalent to myObject['property]
+
+
+// organizing code by grouping
+
+const playerOne = "Tim";
+const playertTwo = "Jenn";
+const playerOneMarker = "X";
+const playerTwoMarker = "O";
+
+// these can be group into objects:
+
+const player1 = {
+    name: "John",
+    marker: "X",
+}
+
+const player2 = {
+    name: "Joanna",
+    marker: "O",
+}
+
+// what can be done on objects (examples):
+
+function printName(player) {
+    console.log(player.name);
+}
+
+// or:
+
+function gameOver(winningPlayer) {
+    console.log("Congratulations!");
+    console.log(winningPlayer.name + " is the winner.");
+}
+
