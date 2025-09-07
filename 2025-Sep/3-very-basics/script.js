982b9fd5a9f6d4c2a7893e5dc918868a6bfadc04
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-07 19:11:04 +0100

    implement grid, add button

--- 2025-Sep/3-very-basics/script.js
+++ 2025-Sep/3-very-basics/script.js
@@ -1,14 +1,22 @@
-
 const age = document.querySelector("#age");
 const result = document.querySelector("#result");
 
-const ageVerified = "Age verification complete."
-const ageLow = "Your age is too low. Sorry."
+const ageVerified = "Age verification complete.";
+// const ageLow = alert("Your age is too low. Sorry.");
 
-function ageResult(age) {
-    if (age >= 18) {
+function ageResult(input) {
+    if (input >= 18) {
         return ageVerified;
     } else {
-        return ageLow;
+        return alert("Your age is too low. Sorry.");
     }
 }
+
+age.addEventListener("change", ()=> {
+    const input = parseInt(age.value);
+    if(isNaN(input)) {
+        result.textContent = "Please use only digts to represent you age.";
+    } else {
+        result.textContent = ageResult(input);
+    }
+})
