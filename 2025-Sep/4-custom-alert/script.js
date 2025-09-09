5df15ee6904b521c5b7fb27fac4f6212ec33c501
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-09 13:23:24 +0100

    finish it later

--- 2025-Sep/4-custom-alert/script.js
+++ 2025-Sep/4-custom-alert/script.js
@@ -1,5 +1,12 @@
-const alertButton = document.querySelector (".alertButton");
+const alertButton = document.querySelector("#alertButton");
+const customAlert = document.querySelector("#customAlert");
+const confirmBtn = document.querySelector("#confirmBtn");
 
-alertButton.addEventListener("click", ()=> {
-    
+
+alertButton.addEventListener("click", function() {
+    customAlert.style.dispaly = 'flex';
+})
+
+confirmBtn.addEventListener("click", function() {
+    customAlert.style.display = "none";
 })
