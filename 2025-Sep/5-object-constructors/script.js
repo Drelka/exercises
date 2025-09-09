b9b9da131135ab1e4614b6819878ceb4faeca70d
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-09 13:54:08 +0100

    quick start

---
+++ 2025-Sep/5-object-constructors/script.js
@@ -0,0 +1,12 @@
+// simple object
+
+const toDoList = {
+    "meeting monday": 1,
+    "meeting tuesday": 3,
+    "tidy up" : myRroom,
+    be: "nice to other peoiple",
+    "when I am": function (tired) {
+        return "go to sleep";
+    },
+}
+
