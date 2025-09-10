b1f14acf876f67517876038d55103cf6148f6d4a
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-10 13:41:56 +0100

    pici polo game beginnings

---
+++ 2025-Sep/5-object-constructors/pici-polo/script.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector("#start");
+const input1 = document.querySelector(".given-number:nth-child(1)");
+const input2 = document.querySelector(".given-number:nth-child(2)");
+
+btn.addEventListener("click", ) {
+}
