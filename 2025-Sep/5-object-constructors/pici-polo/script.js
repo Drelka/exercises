1be40a25d888976652d98a79fbe6e2ff0b92b96f
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-12 11:51:12 +0100

    files moved

--- 2025-Sep/5-object-constructors/pici-polo/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector("#start");
-const input1 = document.querySelector(".given-number:nth-child(1)");
-const input2 = document.querySelector(".given-number:nth-child(2)");
-
-btn.addEventListener("click", ) {
-}
