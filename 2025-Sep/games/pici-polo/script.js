d59aa4b7756cbeca3c03a656178bdad427cf272a
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-12 11:48:55 +0100

    "proper logic implemented"
    Got that logic right, finally.
    I'M BACK!

--- 2025-Sep/games/pici-polo/script.js
+++ 2025-Sep/games/pici-polo/script.js
@@ -3,9 +3,46 @@
 
 
 
-const btn = document.querySelector("#start");
-const you = document.querySelector("#given-number");
-const greg = document.querySelector("#greg > p");
+const checkButton = document.querySelector("#start");
+const givenNum = document.querySelector("#given-number");
+const msg = document.querySelector("#message");
+const revealNum = document.querySelector("#greg > p");
 
-btn.addEventListener("click", ) {
+const randomNum = function() {
+    return Math.floor(Math.random() * 100);
 }
+
+gregsNum = randomNum();
+
+
+const checkNumber = function(numberX) {
+    const num = numberX.value;
+    if (num <= 100 && num > 0) {
+        if (num > gregsNum) {
+            if (num - gregsNum >= 20) {
+                return "Far too high.";
+            } else if (num - gregsNum < 5) {
+                return "Close, but little too high.";
+            } else {
+                return "Number too high.";
+            }
+        } else if (num < gregsNum) {
+            if (gregsNum - num >= 20) {
+                return "Far too low.";
+            } else if (gregsNum - num < 5) {
+                return "Too low, but very close.";
+            } else {
+                return "Number too high.";
+            }
+        } else {
+            return "You got it right!";
+        }
+    } else {
+        return "Choose number from 1 to 100."
+    }
+};
+
+
+checkButton.addEventListener("click", e => {
+    msg.textContent = checkNumber(givenNum);
+});
