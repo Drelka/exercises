38d37587dd887ea08c4af7d2b388ea025d29f0c1
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-12 11:02:32 +0100

    move pici polo to games

---
+++ 2025-Sep/games/pici-polo/script.js
@@ -0,0 +1,11 @@
+// input the number from 1 to 100 as many times as you want until you guess the number that Greg is thinking of
+// Greg will give you hints if your number is higher or lower than his number
+
+
+
+const btn = document.querySelector("#start");
+const you = document.querySelector("#given-number");
+const greg = document.querySelector("#greg > p");
+
+btn.addEventListener("click", ) {
+}
