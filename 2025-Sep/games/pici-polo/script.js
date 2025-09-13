b5a771ba8ecaf706047f9e048edf3719625770bf
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-13 23:36:13 +0100

    not really what I wanted

--- 2025-Sep/games/pici-polo/script.js
+++ 2025-Sep/games/pici-polo/script.js
@@ -4,57 +4,58 @@
 
 
 const checkButton = document.querySelector("#start");
-const givenNum = document.querySelector("#given-number");
+const givenNumInput = document.querySelector("#given-number");
 const msg = document.querySelector("#message");
 const revealNum = document.querySelector("#greg > p");
 
 const randomNum = function() {
-    return Math.floor(Math.random() * 100);
+    return Math.floor(Math.random() * 100) + 1;
 }
 
 const borderFade = function(element) {
     clearTimeout(element.borderFadeTimeout);
-    element.classList.add("fading-red-border");
+    element.classList.add("red-border-flash");
     element.borderFadeTimeout = setTimeout(() => {
         element.classList.remove("fading-red-border");
-    }, 100);
+    }, 1);
 }
 
 gregsNum = randomNum();
 
 const checkNumber = function(numberX) {
     const num = numberX.value;
-    if (givenNum.textContent = "") {
-        borderFade(givenNum);
+    if (num === "") {
+        borderFade(givenNumInput);
+        msg.textContent = "Please enter a number.";
         return;
     }
-    else if (num <= 100 && num > 0) {
-        if (num > gregsNum) {
-            if (num - gregsNum >= 20) {
-                return "Far too high.";
-            } else if (num - gregsNum < 5) {
-                return "Close, but little too high.";
-            } else {
-                return "Number too high.";
-            }
-        } else if (num < gregsNum) {
-            if (gregsNum - num >= 20) {
-                return "Far too low.";
-            } else if (gregsNum - num < 5) {
-                return "Too low, but very close.";
-            } else {
-                return "Number too high.";
-            }
+    else if (num > 100 || num < 1) {
+        borderFade(msg);
+        msg.textContent = "Choose a number from 1 to 100.";
+        return;
+    }
+    else if (num > gregsNum) {
+        if (num - gregsNum >= 20) {
+            msg.textContent = "Far too high.";
+        } else if (num - gregsNum < 5) {
+            msg.textContent = "Close, but little too high.";
         } else {
-            return "You got it right!";
+            msg.textContent = "Number too high.";
+        }
+    } else if (num < gregsNum) {
+        if (gregsNum - num >= 20) {
+            msg.textContent = "Far too low.";
+        } else if (gregsNum - num < 5) {
+            msg.textContent = "Too low, but very close.";
+        } else {
+            msg.textContent = "Number too high.";
         }
     } else {
-        borderFade(msg);
-        return "Choose a number from 1 to 100."
+        msg.textContent = "You got it right!";
     }
 };
 
 
 checkButton.addEventListener("click", e => {
-    msg.textContent = checkNumber(givenNum);
+    checkNumber(givenNumInput);
 });
