689d187b881ed8c4d7a1e66f1e6fcc75d9c69ccc
Drelka <47503020+Drelka@users.noreply.github.com>
2025-09-17 13:38:39 +0100

    convert into add book exercise

--- 2025-Sep/5-object-constructors/script.js
+++ 2025-Sep/5-object-constructors/script.js
@@ -1,40 +1,10 @@
-// const playerOne = "Daniel";
-// const playerTwo = "Natasha";
-// const playerThree = "Agata";
-// const playerFour = "Sonya";
-// const playerFive = "Nathan";
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const Yes = document.querySelector("#Yes");
+const No = document.querySelector("#No");
+const addBookBtn = document.querySelector("#add-book-button")
+const books = document.querySelector("#books");
 
-// const playerOneMarker = "X";
-// const playerTwoMarker = "D";
-// const playerThreeMarker = "J";
-// const playerFourMarker = "Y";
-// const playerFiveMarker = "Z";
 
-// can write like this for each player:
-
-const playerOne = {
-    name: "Daniel",
-    marker: "X",
-
-}
-const playerTwo = {
-    name: "Natasha",
-    marker: "D",
-}
-
-// etc etc...
-// so we can use the function to call the name, or the marker, like this
-
-function printName(player) {
-    console.log(player.name);
-}
-
-console.log(playerOne);
-console.log(playerTwo);
-
-// then we can use this in different situations, letting the condition pick the name, like - winning the game
-
-function gameOver (winner) {
-    console.log(`..and the winner is ${winner}!`);
-}
 
